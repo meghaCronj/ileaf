1a329e4566ed61d0122329fd83b2695219088075
Megha Bhargava <megha@cronj.com>
2021-07-09 16:57:58 +0530

    login and logout

--- auth/auth.js
+++ auth/auth.js
@@ -1,30 +1,24 @@
-const passport = require("passport");
-const passportJWT = require("passport-jwt");
+const jwt = require("jsonwebtoken");
+const Participants = require("../models/participants.model");
 
-let JwtStrategy = passportJWT.Strategy;
-let ExtractJwt = passportJWT.ExtractJwt;
-const User = require("../models/user.model");
-const opts = {
-  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-  secretOrKey: process.env.JWT_SECRET,
+const auth = async (req, res, next) => {
+  try {
+    const token = req.header("Authorization").replace("Bearer ", "");
+    const decoded = jwt.verify(token, "justanothertoken");
+    const participants = await Participants.findOne({
+      _id: decoded._id,
+      "tokens.token": token,
+    });
+
+    if (!participants) {
+      throw new Error();
+    }
+    req.token = token;
+    req.participants = participants;
+    next();
+  } catch (e) {
+    res.status(401).send({ error: "Please authenticate" });
+  }
 };
-exports.autherization = () => {
-  // console.log("Inside Authorization")
-  passport.use(
-    new JwtStrategy(opts, function (jwt_payload, done) {
-      User.findOne({ _id: jwt_payload.id })
-        .then((user) => {
-          if (user) {
-            return done(null, user);
-          } else {
-            // console.log("unauthenticated");sa
-            return done(null, false);
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-          return err;
-        });
-    })
-  );
-};
+
+module.exports = auth;
